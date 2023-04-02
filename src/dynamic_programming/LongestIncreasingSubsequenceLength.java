e5370173b2a190c8230ce59d4acc9f46596df642
Shivam Sharma <shivamsharma.ade@gmail.com>
2023-04-02 13:38:34 +0530

    dynamic programming - Longest Increasing Subsequence using binary search

---
+++ src/dynamic_programming/LongestIncreasingSubsequenceLength.java
@@ -0,0 +1,24 @@
+package dynamic_programming;
+
+/*
+    https://leetcode.com/problems/longest-increasing-subsequence/description/
+ */
+public class LongestIncreasingSubsequenceLength {
+    public static int lengthOfLIS(int[] nums) {
+        int n = nums.length, dp[] = new int[n], ans = 0;
+        for (int i=0; i<n; i++) {
+            int low = 0, high = ans;
+            while (low != high) {
+                int mid = low + (high - low)/2;
+                if (nums[i] <= dp[mid]) {
+                    high = mid;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            dp[low] = nums[i];
+            if (low == ans) ans++;
+        }
+        return ans;
+    }
+}
