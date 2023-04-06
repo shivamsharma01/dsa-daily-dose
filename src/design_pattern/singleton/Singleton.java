e66b2736388e4e1c1cc3a26868f5ec01135a97ec
Shivam Sharma <shivamsharma.ade@gmail.com>
2023-04-06 06:51:13 +0530

    Design Pattern - Singleton design pattern working example added

---
+++ src/design_pattern/singleton/Singleton.java
@@ -0,0 +1,29 @@
+package design_pattern.singleton;
+
+public class Singleton {
+
+    // volatile helps overcome the race condition in a multithreaded environment
+    private static volatile Singleton singleton = null;
+
+    private Singleton(){
+        // useful if reflection is used to bypass the accessibility (private/public)
+        if(singleton != null){
+            throw new RuntimeException("Exception !!!!");
+        }
+    }
+    public static Singleton getInstance(){
+        if(singleton == null){
+            // Single instance throughout the application level
+            synchronized (Singleton.class){
+                // inner null check is necessary
+                // suppose before any thread actually initializes the singleton variable below,
+                // multiple threads have got true for singleton == null
+                // then without the inner null check, they all will be able to create a new instance
+                if(singleton == null){
+                    singleton = new Singleton();
+                }
+            }
+        }
+        return singleton;
+    }
+}
