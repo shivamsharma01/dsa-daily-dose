04e36ff826d0e9c7e633f183508563a5693e1f33
Shivam Sharma <shivamsharma.ade@gmail.com>
2023-04-12 08:32:14 +0530

    Sorting - BubbleSort fixed

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -8,7 +8,7 @@ public class BubbleSort {
     public static void sort(int arr[]) {
         int n = arr.length;
         boolean isSorted;
-        for (int i=0; i<n-1; i++) {
+        for (int i=0; i<n; i++) {
             isSorted = true;
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j+1]) {
