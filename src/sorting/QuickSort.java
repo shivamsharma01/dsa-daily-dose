7acd493cfa2c34758fb72c6d8753b1094e5436f3
Shivam Sharma <shivamsharma.ade@gmail.com>
2023-04-12 08:08:51 +0530

    Sorting - QuickSort fixed

--- src/sorting/QuickSort.java
+++ src/sorting/QuickSort.java
@@ -1,30 +1,29 @@
 package sorting;
 
+import util.ArrayUtil;
+
 import java.util.Arrays;
 
 public class QuickSort {
 
     private static int partition(int arr[], int low, int high) {
-        int i = -1, val = arr[high], j = 0;
-
+        int i = low-1, val = arr[high], j = low;
         while (j < high) {
             if (arr[j] <= val) {
-                int temp = arr[j];
-                arr[j] = arr[++i];
-                arr[i] = temp;
+                ArrayUtil.swap(arr, j, ++i);
             }
             j++;
         }
-
-        arr[++i] = val;
+        arr[high] = arr[++i];
+        arr[i] = val;
         return i;
     }
 
-    public static void quickSort(int arr[], int low, int high) {
+    public static void sort(int arr[], int low, int high) {
         if(low < high) {
             int p = partition(arr, low, high);
-            quickSort(arr, low, p - 1);
-            quickSort(arr,p + 1, high);
+            sort(arr, low, p - 1);
+            sort(arr,p + 1, high);
         }
     }
 }
@@ -32,9 +31,9 @@ public class QuickSort {
 
 class QuickSortRunner {
     public static void main(String[] args) {
-        int arr[] = new int[] {21, 5, 1, 4, 2, 9, 3, 1, 14, 17, 4, 22};
+        int arr[] = new int[] {21, 5, 1, 4, 2, 9, 3, 1, 14, 17, 4, 1};
         System.out.println(Arrays.toString(arr));
-        MergeSort.sort(arr, 0, arr.length-1);
+        QuickSort.sort(arr, 0, arr.length-1);
         System.out.println(Arrays.toString(arr));
     }
 }
