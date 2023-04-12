8eef14f955d4ebb9765b975e6ea12eeda1e46107
Shivam Sharma <shivamsharma.ade@gmail.com>
2023-04-12 08:11:34 +0530

    Sorting - HeapSort & BubbleSort

---
+++ src/sorting/BubbleSort.java
@@ -0,0 +1,32 @@
+package sorting;
+
+import util.ArrayUtil;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void sort(int arr[]) {
+        int n = arr.length;
+        boolean isSorted;
+        for (int i=0; i<n-1; i++) {
+            isSorted = true;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    isSorted = false;
+                    ArrayUtil.swap(arr, j, j+1);
+                }
+            }
+            if (isSorted) break;
+        }
+    }
+
+}
+
+class BubbleSortRunner {
+    public static void main(String[] args) {
+        int arr[] = new int[] {21, 5, 1, 4, 2, 9, 3, 1, 14, 17, 4, 22};
+        System.out.println(Arrays.toString(arr));
+        BubbleSort.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
---
+++ src/sorting/HeapSort.java
@@ -0,0 +1,49 @@
+package sorting;
+
+import util.ArrayUtil;
+
+import java.util.Arrays;
+
+public class HeapSort {
+
+    public static void sort(int arr[]) {
+        int n = arr.length;
+
+        // build heap
+        for (int i=n/2-1; i>=0; i--)
+            heapify(arr, i, n);
+
+        // heap sort
+        for (int i=n-1; i>=0; i--) {
+            ArrayUtil.swap(arr, 0, i);
+            heapify(arr, 0, i);
+        }
+    }
+
+    private static void heapify(int arr[], int i, int n) {
+        int indexWithLargestValue = i;
+        int left = i * 2 + 1;
+        int right = i * 2 + 2;
+
+        if (left < n && arr[left] > arr[indexWithLargestValue])
+            indexWithLargestValue = left;
+
+        if (right < n && arr[right] > arr[indexWithLargestValue])
+            indexWithLargestValue = right;
+
+        if (i != indexWithLargestValue) {
+            ArrayUtil.swap(arr, i, indexWithLargestValue);
+            heapify(arr, indexWithLargestValue, n);
+        }
+    }
+
+}
+
+class HeapSortRunner {
+    public static void main(String[] args) {
+        int arr[] = new int[] {21, 5, 1, 4, 2, 9, 3, 1, 14, 17, 4, 22};
+        System.out.println(Arrays.toString(arr));
+        HeapSort.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
