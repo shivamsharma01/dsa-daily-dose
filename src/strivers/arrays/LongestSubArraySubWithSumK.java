1d2dffb2b0036d832f25caa9f71f1265a42f9c6d
Shivam Sharma <shivamsharma.ade@gmail.com>
2023-04-17 19:15:00 +0530

    Strivers - Arrays - LongestSubArraySubWithSumK - refactored

--- src/strivers/arrays/LongestSubArraySubWithSumK.java
+++ src/strivers/arrays/LongestSubArraySubWithSumK.java
@@ -5,21 +5,20 @@ import java.util.Map;
 
 public class LongestSubArraySubWithSumK {
 
-    public static int lenOfLongSubarr(int A[], int N, int K) {
-        long prefixSum[] = new long[N];
+    public static int lenOfLongSubArr(int A[], int N, int K) {
+        long prefixSum = A[0];
         Map<Long, Integer> map = new HashMap<>();
 
         int lengthOfLongestSubArrayWithSumK = A[0] == K ? 1 : 0;
 
-        prefixSum[0] = A[0];
-        map.put(prefixSum[0], 0);
+        map.put(prefixSum, 0);
 
         for (int i=1; i<N; i++) {
-            prefixSum[i] = prefixSum[i-1]+A[i];
-            if (!map.containsKey(prefixSum[i])) map.put(prefixSum[i], i);
-            if(prefixSum[i] == K) lengthOfLongestSubArrayWithSumK = i+1;
-            else if (map.containsKey(prefixSum[i]-K)) {
-                lengthOfLongestSubArrayWithSumK = Math.max(lengthOfLongestSubArrayWithSumK, i - map.get(prefixSum[i]-K));
+            prefixSum += A[i];
+            if (!map.containsKey(prefixSum)) map.put(prefixSum, i);
+            if(prefixSum == K) lengthOfLongestSubArrayWithSumK = i+1;
+            else if (map.containsKey(prefixSum-K)) {
+                lengthOfLongestSubArrayWithSumK = Math.max(lengthOfLongestSubArrayWithSumK, i - map.get(prefixSum-K));
             }
         }
 
@@ -28,6 +27,6 @@ public class LongestSubArraySubWithSumK {
 
     public static void main(String[] args) {
         int arr[] = new int[] {4, 2, 5, 3, 1, 0, -1};
-        System.out.println(lenOfLongSubarr(arr, arr.length, 8));
+        System.out.println(lenOfLongSubArr(arr, arr.length, 8));
     }
 }
