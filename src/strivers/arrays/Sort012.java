e28b799eddb04aadf0da8ad480a6f2095eade43f
Shivam Sharma <shivamsharma.ade@gmail.com>
2023-04-23 20:33:02 +0530

    Strivers Arrays - 2 Sum - Sort 0, 1 & 2 - Majority Element - Kadane Algo

---
+++ src/strivers/arrays/Sort012.java
@@ -0,0 +1,38 @@
+package strivers.arrays;
+
+import java.util.Arrays;
+
+public class Sort012 {
+
+    public static void main(String[] args) {
+        int arr[] = new int[]{0, 1, 1, 1, 0, 2, 1, 2, 0};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    /*
+        Dutch national flag algorithm
+    */
+    private static void sort(int arr[]) {
+        int low = 0, mid = 0, high = arr.length - 1;
+        while (mid < high) {
+            if (arr[mid] == 0) {
+                swap(arr, low, mid);
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else {
+                swap(arr, mid, high);
+                high--;
+            }
+        }
+    }
+
+    private static void swap(int arr[], int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+}
