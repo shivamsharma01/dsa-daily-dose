0f5faaa6a5c3078edae3bda5366b08992579eb1b
Shivam Sharma <shivamsharma.ade@gmail.com>
2023-04-25 12:39:30 +0530

    Strivers Array - IsRotatedSorted - LargestElement - SecondLargest - RemoveDuplicatedFromSorted

---
+++ src/strivers/arrays/IsRotatedSorted.java
@@ -0,0 +1,13 @@
+package strivers.arrays;
+
+public class IsRotatedSorted {
+
+    public boolean check(int[] nums) {
+        int count = 0;
+        for (int i=0; i<nums.length-1; i++) {
+            if (nums[i] > nums[i+1]) count++;
+        }
+        return count == 0 || count == 1 && nums[0] >= nums[nums.length-1];
+    }
+
+}
---
+++ src/strivers/arrays/LargestElement.java
@@ -0,0 +1,11 @@
+package strivers.arrays;
+
+public class LargestElement {
+
+    public int largest(int arr[], int n) {
+        int max = arr[0];
+        for (int elm: arr) max = Math.max(max, elm);
+        return max;
+    }
+
+}
